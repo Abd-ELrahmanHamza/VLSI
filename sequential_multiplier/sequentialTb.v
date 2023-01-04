0652041e56e84bdd01438dbd2b385a1bdd78440f
ZiadSheriif <zsherif308@gmail.com>
2023-01-04 05:22:44 +0200

    run test cases of sequential multiplier

--- sequential_multiplier/sequentialTb.v
+++ sequential_multiplier/sequentialTb.v
@@ -17,17 +17,17 @@ module sequentialmultiplierTB;
     failed = 0;
     clk = 0;
     en = 1;
-    a = 5;
-    b = -7;
+    a = 553524;
+    b = 840;
     reset = 1;
     #20;
     reset = 0;
     #20;
     #640;
-    a = 2;
-    b = 3;
+    a = 1348760118;
+    b = 1348543286;
     #20;
-    if (sequentialmultiplierResult === -35) begin
+    if (sequentialmultiplierResult === 464960160) begin
       passed = passed + 1;
       $display("TestCase#1: success");
     end else begin
@@ -36,10 +36,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = -12;
-    b = -4;
+    a = 553524;
+    b = -259;
     #20;
-    if (sequentialmultiplierResult === 6) begin
+    if (sequentialmultiplierResult === 64'h193DE4CED7437964) begin
       passed = passed + 1;
       $display("TestCase#2: success");
     end else begin
@@ -48,10 +48,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = -9;
-    b = 5;
+    a = 1348760118;
+    b = -1199060305;
     #20;
-    if (sequentialmultiplierResult === 48) begin
+    if (sequentialmultiplierResult === -143362716) begin
       passed = passed + 1;
       $display("TestCase#3: success");
     end else begin
@@ -60,10 +60,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = 11;
-    b = 0;
+    a = -259;
+    b = 553524;
     #20;
-    if (sequentialmultiplierResult === -45) begin
+    if (sequentialmultiplierResult === 64'hE98E647F4142AEEA) begin
       passed = passed + 1;
       $display("TestCase#4: success");
     end else begin
@@ -72,10 +72,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = 10;
-    b = 1;
+    a = 32'hB887CAAF;
+    b = 32'h50647236;
     #20;
-    if (sequentialmultiplierResult === 0) begin
+    if (sequentialmultiplierResult === -143362716) begin
       passed = passed + 1;
       $display("TestCase#5: success");
     end else begin
@@ -84,10 +84,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = 4;
-    b = 6;
+    a = -259;
+    b = -259;
     #20;
-    if (sequentialmultiplierResult === 10) begin
+    if (sequentialmultiplierResult === 64'hFDC57DE4938342EA) begin
       passed = passed + 1;
       $display("TestCase#6: success");
     end else begin
@@ -96,10 +96,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
-    a = -1;
-    b = -7;
+    a = -1199060305;
+    b = -2005095693;
     #20;
-    if (sequentialmultiplierResult === 24) begin
+    if (sequentialmultiplierResult === 67081) begin
       passed = passed + 1;
       $display("TestCase#7: success");
     end else begin
@@ -108,8 +108,10 @@ module sequentialmultiplierTB;
                sequentialmultiplierResult);
     end
     #640;
+    a = 1348760118;
+    b = 1;
     #20;
-    if (sequentialmultiplierResult === 7) begin
+    if (sequentialmultiplierResult === 64'h215D8B0A7A419A1D) begin
       passed = passed + 1;
       $display("TestCase#8: success");
     end else begin
@@ -117,7 +119,53 @@ module sequentialmultiplierTB;
       $display("TestCase#8: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b,
                sequentialmultiplierResult);
     end
+    #640;
+    a = -1199060305;
+    b = 1;
+    #20;
+    if (sequentialmultiplierResult === 1348760118) begin
+      passed = passed + 1;
+      $display("TestCase#9: success");
+    end else begin
+      failed = failed + 1;
+      $display("TestCase#9: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b,
+               sequentialmultiplierResult);
+    end
+    #640;
+    a = 0;
+    b = 1348760118;
+    #20;
+    if (sequentialmultiplierResult === -1199060305) begin
+      passed = passed + 1;
+      $display("TestCase#10: success");
+    end else begin
+      failed = failed + 1;
+      $display("TestCase#10: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b,
+               sequentialmultiplierResult);
+    end
+    #640;
+    a = -1199060305;
+    b = 0;
+    #20;
+    if (sequentialmultiplierResult === 0) begin
+      passed = passed + 1;
+      $display("TestCase#11: success");
+    end else begin
+      failed = failed + 1;
+      $display("TestCase#11: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b,
+               sequentialmultiplierResult);
+    end
+    #20;
+    if (sequentialmultiplierResult === 0) begin
+      passed = passed + 1;
+      $display("TestCase#12: success");
+    end else begin
+      failed = failed + 1;
+      $display("TestCase#12: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b,
+               sequentialmultiplierResult);
+    end
     $display("Total passed tests: %d and Total failed tests: %d", passed, failed);
+
   end
   always begin
     #10;
